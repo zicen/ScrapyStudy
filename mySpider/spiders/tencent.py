a3d88251b48bbd698893b7fd7be123b24f2091df
zicen <1140377034@qq.com>
2017-12-20 17:29:47 +0800

    腾讯招聘自动翻页爬虫

--- mySpider/spiders/tencent.py
+++ mySpider/spiders/tencent.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
-import scrapy
 from mySpider.items import TencentItem
+import scrapy
 import re
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 
 class TencentSpider(scrapy.Spider):
@@ -12,18 +16,16 @@ class TencentSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        # open("tencent.html", "wb").write(response.body)
-        items = []
         for each in response.xpath('//*[@class="even"]'):
             item = TencentItem()
-            name = ""
-            if len(each.xpath('a').extract()) > 0:
-                name = each.xpath('a').extract()[0]
+            name = each.xpath('./td[1]/a/text()').extract()[0]
             detailLink = each.xpath('./td[1]/a/@href').extract()[0]
             positionInfo = each.xpath('./td[2]/text()').extract()[0]
             peopleNumber = each.xpath('./td[3]/text()').extract()[0]
             workLocation = each.xpath('./td[4]/text()').extract()[0]
             publishTime = each.xpath('./td[5]/text()').extract()[0]
+
+            # print name, detailLink, catalog, peopleNumber, workLocation,publishTime
 
             item['name'] = name.encode('utf-8')
             item['detailLink'] = detailLink.encode('utf-8')
@@ -31,6 +33,13 @@ class TencentSpider(scrapy.Spider):
             item['peopleNumber'] = peopleNumber.encode('utf-8')
             item['workLocation'] = workLocation.encode('utf-8')
             item['publishTime'] = publishTime.encode('utf-8')
-            items.append(item)
 
-        return items
+            curpage = re.search('(\d+)', response.url).group(1)
+            page = int(curpage) + 10
+            url = re.sub('\d+', str(page), response.url)
+
+            # 发送新的url请求加入待爬队列，并调用回调函数 self.parse
+            yield scrapy.Request(url, callback=self.parse)
+
+            # 将获取的数据交给pipeline
+            yield item
